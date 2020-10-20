9be0c19909cc17e82f16aff8ae171011ae942ab7
werren-m <w_erren@hotmail.com>
2020-10-20 10:20:37 +0700

    cors

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const errorHandler = require('./middlewares/errorHandler')
 // console.log(process.env.PORT)
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use('/uploads', express.static('uploads'));
